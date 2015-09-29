84a7f2f9fc68f3cbbff1f3c2d0530b84741f0f17
Stefan Billeb <stefan.billeb@gmx.net>
2015-09-29 15:14:22 +0200

    Add output of secret number

--- guessing_game/src/main.rs
+++ guessing_game/src/main.rs
@@ -7,6 +7,7 @@ fn main() {
     println!("Guess the number!");
     
     let secret_number = rand::thread_rng().gen_range(1,101);
+    println!("Secret number is {}", secret_number);
 
     println!("Please input your guess.");
 
